2b62d1bb341af40c46f73f93787e3cb9e0e00caa
imfsdev <mingsong.webdev@gmail.com>
2020-12-24 21:00:06 +0800

    add post url functionality

--- src/pages/FlightSearchPage/components/FlightSearchForm/FlightSearchForm.js
+++ src/pages/FlightSearchPage/components/FlightSearchForm/FlightSearchForm.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Row, Col, Button, DatePicker, Select } from 'antd';
 import cx from 'classnames';
+import moment from 'moment';
+import { isEmpty } from 'lodash';
 
-import RightIconInput from 'components/RightIconInput/RightIconInput';
 import NumberInput from 'components/NumberInput/NumberInput';
 import { Checkbox } from 'components/CheckboxGroup/CheckboxGroup';
+import Complete from 'components/AutoComlete/AutoComplete';
 
 import headerImg from 'images/withBlue.png';
 import { ReactComponent as SearchIcon } from 'icons/search.svg';
@@ -15,7 +17,81 @@ import { ReactComponent as PlusIcon } from 'icons/plusBlue.svg';
 import styles from './FlightSearchForm.module.scss';
 
 const FlightSearchForm = () => {
-  const [searchFrom, setSearchFrom] = useState('');
+  const [departureDate, setDepartureDate] = useState(moment().toISOString());
+  const [returnDate, setReturnDate] = useState(moment().format('MM-DD-YYYY'));
+  const [searchType, setSearchType] = useState('roundtrip');
+  const [cabinClass, setCabinClass] = useState('');
+  const [travelerCount, setTravelerCount] = useState(1);
+  const [nonStopFlight, setNonStopFlight] = useState(false);
+  const [origin, setOrigin] = useState({});
+  const [destination, setDestination] = useState({});
+  const originCache = useRef({});
+  const destinationCache = useRef({});
+
+  const cabinClassOptions = [
+    {
+      value: 'noPref',
+      title: 'No Preference',
+    },
+    {
+      value: 'economy',
+      title: 'Economy/Coach',
+    },
+    {
+      value: 'premiumEconomy',
+      title: 'Premium Economy',
+    },
+    {
+      value: 'business',
+      title: 'Business',
+    },
+    {
+      value: 'first',
+      title: 'First',
+    },
+  ];
+
+  const handleTravelerCountChange = () => (value) => {
+    setTravelerCount(value);
+  };
+
+  const clearOriginData = () => {
+    if (origin?.location_id) {
+      originCache.current = origin || {};
+    }
+    setOrigin({});
+  };
+  const clearDestinationData = () => {
+    if (destination?.location_id) {
+      destinationCache.current = destination || {};
+    }
+    setDestination({});
+  };
+
+  const validateForm = () => {
+    if (isEmpty(origin) || isEmpty(destination) || departureDate === '' || returnDate === '' || travelerCount <= 0)
+      return false;
+    return true;
+  };
+
+  const postSearch = () => {
+    const validate = validateForm();
+    if (validate) {
+      // const originCity = `${origin.location_name} ${getName(origin.province)}`;
+      // const destinationCity = `${destination.location_name} ${getName(destination.iso_country_code)}`;
+      // const originAirCode
+      // const destinationAirCode
+      const refId = 10045;
+      const roundTripPost = `https://secure.rezserver.com/flights/results/depart/?rs_chk_in=${departureDate}&rs_chk_out=${departureDate}&rs_adults=${travelerCount}&rs_children=0&refid=${refId}&air_search_type=${searchType}&cabin_class=`;
+      const oneWayPost = `https://secure.rezserver.com/flights/results/depart/?rs_chk_in1=${departureDate}&rs_chk_out1=${departureDate}&rs_adults=${travelerCount}&rs_children=0&refid=${refId}&air_search_type=${searchType}&cabin_class=`;
+      if (searchType === 'roundtrip') {
+        window.open(roundTripPost);
+      } else {
+        window.open(oneWayPost);
+      }
+    }
+  };
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.main}>
@@ -36,8 +112,18 @@ const FlightSearchForm = () => {
                 </div>
                 <div className={styles.switchType}>
                   <Button.Group>
-                    <Button className={styles.roundBtn}>Round Trip</Button>
-                    <Button className={styles.roundBtn}>One-Way</Button>
+                    <Button
+                      className={searchType === 'roundtrip' ? styles.roundSelectedBtn : styles.roundBtn}
+                      onClick={() => setSearchType('roundtrip')}
+                    >
+                      Round Trip
+                    </Button>
+                    <Button
+                      className={searchType === 'oneway' ? styles.roundSelectedBtn : styles.roundBtn}
+                      onClick={() => setSearchType('oneway')}
+                    >
+                      One-Way
+                    </Button>
                   </Button.Group>
                 </div>
               </div>
@@ -47,23 +133,20 @@ const FlightSearchForm = () => {
             <Col flex="1" className={styles.searchCol}>
               <div className={styles.searchForm}>
                 <div className={styles.searchText}>From</div>
-                <div className={styles.searchInput}>
-                  <RightIconInput
-                    name="Search or enter location..."
-                    value={searchFrom}
-                    onChange={setSearchFrom}
-                    rightComponent={
-                      <div className={styles.rightIconWrapper}>
-                        <PinIcon className={styles.pinIcon} width={24} height={24} />
-                      </div>
-                    }
+                <div className={styles.autoCompleteWrapper}>
+                  <Complete
+                    value={origin}
+                    onSelect={(place) => setOrigin(place)}
+                    clearData={clearOriginData}
+                    placeholder="Search or enter location..."
                   />
+                  <PinIcon className={styles.pinIcon} width={24} height={24} />
                 </div>
                 <div className={styles.datePickerWrapper}>
                   <DatePicker
-                    //   onChange={(e) => onDateChange('start_date')(e)}
-                    //   defaultValue={moment(data.start_date)}
-                    //   value={moment(data.start_date)}
+                    onChange={(value) => setDepartureDate(moment(value).format('MM-DD-YYYY'))}
+                    defaultValue={moment()}
+                    value={moment(departureDate)}
                     format="MM-DD-YYYY"
                     suffixIcon={<CalendarIcon className="calendarIcon" width={20} height={20} />}
                     className={styles.datePicker}
@@ -75,16 +158,23 @@ const FlightSearchForm = () => {
             <Col flex="1" className={styles.searchCol}>
               <div className={styles.searchForm}>
                 <div className={styles.searchText}>To</div>
-                <div className={styles.searchInput}>
-                  <RightIconInput
-                    name="Search or enter location..."
-                    value={searchFrom}
-                    onChange={setSearchFrom}
-                    rightComponent={
-                      <div className={styles.rightIconWrapper}>
-                        <PinIcon className={styles.pinIcon} />
-                      </div>
-                    }
+                <div className={styles.autoCompleteWrapper}>
+                  <Complete
+                    value={destination}
+                    onSelect={(place) => setDestination(place)}
+                    clearData={clearDestinationData}
+                    placeholder="Search or enter location..."
+                  />
+                  <PinIcon className={styles.pinIcon} width={24} height={24} />
+                </div>
+                <div className={styles.datePickerWrapper}>
+                  <DatePicker
+                    onChange={(value) => setReturnDate(moment(value).format('MM-DD-YYYY'))}
+                    defaultValue={moment()}
+                    value={moment(returnDate)}
+                    format="MM-DD-YYYY"
+                    suffixIcon={<CalendarIcon className="calendarIcon" width={20} height={20} />}
+                    className={styles.datePicker}
                   />
                 </div>
               </div>
@@ -110,13 +200,16 @@ const FlightSearchForm = () => {
                   <div className={styles.searchSelectWrapper}>
                     <Select
                       placeholder="Select..."
-                      // value={search}
-                      // options={searchOptions}
-                      // mode="tags"
-                      // defaultValue={search}
-                      // onChange={handleChange('search')}
+                      value={cabinClass}
+                      onChange={(value) => setCabinClass(value)}
                       className={styles.searchSelect}
-                    />
+                    >
+                      {cabinClassOptions.map((option) => (
+                        <Select.Option key={option.value} value={option.value}>
+                          {option.title}
+                        </Select.Option>
+                      ))}
+                    </Select>
                   </div>
                 </Col>
               </Row>
@@ -133,8 +226,9 @@ const FlightSearchForm = () => {
                       {/* <NumberInput className={styles.travelers} name="travelers" onChange={onChange} defaultValue={2} /> */}
                       <NumberInput
                         className={styles.travelers}
-                        name="travelers"
                         defaultValue={1}
+                        name="travelerCount"
+                        onChange={handleTravelerCountChange}
                         leftComponent={
                           <div>
                             <MinusIcon width={46} height={46} />
@@ -155,8 +249,15 @@ const FlightSearchForm = () => {
                   </Row>
                   <Row>
                     <div className={styles.searchComp}>
-                      {/* <Checkbox invert value={tosAgreed} onChange={toggleTosAgreed}> */}
-                      <Checkbox invert size="large">
+                      <Checkbox
+                        invert
+                        size="large"
+                        checked={nonStopFlight}
+                        onChange={(e) => {
+                          e.preventDefault();
+                          setNonStopFlight(!nonStopFlight);
+                        }}
+                      >
                         <span className={styles.checkLabel}>Preferred</span>
                       </Checkbox>
                     </div>
@@ -167,12 +268,10 @@ const FlightSearchForm = () => {
           </Row>
           <Row justify="center">
             <Col md={24} xs={24} justify="center" className={styles.buttonGroup}>
-              <a href="null">
-                <Button className={styles.searchBtn}>
-                  <SearchIcon width={24} height={24} />
-                  <span className={styles.searchBtnText}>Search Flights</span>
-                </Button>
-              </a>
+              <Button className={styles.searchBtn} onClick={() => postSearch()}>
+                <SearchIcon width={24} height={24} />
+                <span className={styles.searchBtnText}>Search Flights</span>
+              </Button>
             </Col>
           </Row>
         </div>
