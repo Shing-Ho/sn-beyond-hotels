643fda69a1d75654e24be5ae55a0401a99754ca0
webdevboy <aron@simplenight.com>
2021-01-08 16:23:38 +0000

    test signed commit
    Signed-off-by: webdevboy <aron@simplenight.com>

--- src/pages/Venues/components/VenuesAvailability/VenuesAvailability.js
+++ src/pages/Venues/components/VenuesAvailability/VenuesAvailability.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'antd';
 
 import styles from './VenuesAvailability.module.scss';
 
 export default function VenuesAvailability() {
+  const [mode, setMode] = useState(false);
   return (
     <div className={styles.availability}>
       <h3 className={styles.title}>Availability</h3>
-      <p className={styles.description}>Add difference times this venue is open or available by adding rules.</p>
-      <Button className={styles.btn}>
+
+      {mode && <div>aaa</div>}
+      {!mode && (
+        <div className={styles.contentContainer}>
+          <p className={styles.description}>Add difference times this venue is open or available by adding rules.</p>
+        </div>
+      )}
+      <Button className={styles.btn} onClick={() => setMode(!mode)}>
         <i className="fa fa-plus" aria-hidden="true" /> Add Rule
       </Button>
     </div>
